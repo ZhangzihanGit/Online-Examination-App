1fcb5cac81515199880eb0bda0e39bb6f119f929
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2020-09-09 22:02:38 +1000

    bugfix: remove back slash when sending fetch request

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState("");
+
+  const handleClick = async () => {
+    try {
+      const resposne = await fetch('login');
+      const msg = await resposne.text();
+      console.log(msg);
+      setData(msg);
+    } catch {
+      console.log('fetch data error');
+    }
+  }
+
   return (
     <div className="App">
-      hello world
+      hello world from react
+
+      <br />
+      <button onClick={handleClick}>Get</button>
+      <br />
+      {`From server: ${data}`}
     </div>
   );
 }
