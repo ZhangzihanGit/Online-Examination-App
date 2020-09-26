a97cf6601b09b4926819137f677ff78dc62f528c
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2020-09-26 17:46:57 +1000

    feat: presistent the state

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import throttle from 'lodash/throttle';
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
+import { loadState, saveState } from './localStorage';
 import App from './App';
 
-const store = configureStore();
+const persistedState = loadState();
+const store = configureStore(persistedState);
+
+/**
+ * By wrapping callback in a throttle call, we can insure 
+ * that the inner function we pass is not going to be called 
+ * more often than our specified number of milliseconds.
+ */
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}, 1000));
+
+/** only store user state in localStorage ? */
+// store.subscribe(() => {
+//   saveState({
+//     user: store.getState().user,
+//   });
+// });
 
 ReactDOM.render(
   <Provider store={store}>
